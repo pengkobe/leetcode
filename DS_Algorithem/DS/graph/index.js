2eb0a06d3a9dfa0c33e7531ff405d5d37edd12fb
pengkobe <yipeng.info@gmail.com>
2017-01-23 23:22:53 +0800

    add notes for topsort

--- DS_Algorithem/DS/graph/index.js
+++ DS_Algorithem/DS/graph/index.js
@@ -109,3 +109,33 @@ function pathTo(v){
 function hasPathTo(){
   return this.marked[v];
 }
+
+/*拓扑排序*/
+function topSort(){
+  var stack = [];
+  var visited = [];
+  for (var i = 0; i < this.vertices; i++) {
+      visited[i] = false;
+  }
+  for (var i = 0; i < this.vertices; i++) {
+     if(visited[i] = false){
+        this.topSortHelper(i, visited, stack);
+     }
+  }
+  for (var i = 0; i < this.vertices; i++) {
+    if(stack[i] != undefined && stack[i] != false){
+      print(this.vertexList[stack[i]]);
+      visited[i] = false;
+    }
+  }
+}
+
+function topSortHelper(v, visited, stack){
+  visited[v] = true;
+  for each(var w in this.adj[v]){
+    if(!visited[w]){
+      this.topSortHelper(visited[w], visited, stack);
+    }
+  }
+  stack.push(v);
+}

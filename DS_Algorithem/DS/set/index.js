baba628c5f97d2ef9f35034834373caa20d22a1e
pengkobe <yipeng.info@gmail.com>
2017-01-28 21:38:28 +0800

    add notes for define_set

---
+++ DS_Algorithem/DS/set/index.js
@@ -0,0 +1,12 @@
+function Set(){
+  this.dataStore =[];
+  this.add = add;
+  this.remove = remove;
+  this.size = size;
+  this.union = union;
+  this.intersect = intersect;
+  this.subset = subset;
+  this.difference = difference;
+  this.show = show;
+}
+

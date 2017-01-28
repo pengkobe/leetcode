c6e98fe8cf38d32a42e652a00ffe1e9de0d8386d
pengkobe <yipeng.info@gmail.com>
2017-01-28 21:19:10 +0800

    add notes for hash_collision_detection

--- DS_Algorithem/DS/hash/index.js
+++ DS_Algorithem/DS/hash/index.js
@@ -39,3 +39,56 @@ function betterHash(string, arr){
   total = total % arr.length;
   return parseInt(total);
 }
+
+/* 散列化整型键 */
+function getRandomInt(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+/* 生成学生成绩数据(用于检验哪个检测算法好) */
+function genStuData(arr){
+  for (var i = 0; i < arr.length; i++) {
+    var num = "";
+    for (var j = 0; j < 9; j++) {
+      num += Math.floor(Math.random() * 10)；
+    }
+    num += getRandomInt(50, 100);
+    arr[i] = num;
+  }
+}
+
+/* 碰撞处理 */
+function buildChains(){
+  for (var i = 0; i < this.table.length; i++) {
+    this.table[i] = new Array();
+  }
+}
+function put(key,data){
+  var pos = this.betterHash(key);
+  var index  = 0;
+  if(this.table[pos][index] == undefined){
+    this.table[pos][index+1] = data;
+  ++index;
+
+  }else{
+    while(this.table[pos][index] != undefined){
+      ++index;
+    }
+    this.table[pos][index + 1] = data;
+  }
+}
+
+function get(key){
+  var index = 0;
+  var hash = this.betterHash(key);
+  if(this.table[pos][index] == key){
+    return this.table[pos][index+1];
+  }
+  index +=2;
+  else{
+    while(this.table[pos][index] != key){
+      index +=2;
+    }
+    return this.table[pos][index+1];
+  }
+}
+

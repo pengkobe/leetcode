9b2b9e8d15dd0525783bc828e1edbee6fa9931ca
kobepeng <pengkobe123@gmail.com>
2017-07-31 21:31:23 +0800

    start programming of String_to_Integer

--- questions/String_to_Integer.py
+++ questions/String_to_Integer.py
@@ -25,6 +25,75 @@
 
 
 # 考虑事项
+# 1. 开头为 + - 号
+# 2. 只有 + - 号
+# 3. str 为空
+# 4. str 为非法字符串
+# 5. str 超过最大值与最小值
+# 
 
 def myAtoi(str):
-    return 1;
+    ret = 0;
+    INT_MAX = 2147483647;  
+    INT_MIN = -2147483648;
+    a_0 = ord('0')  
+    a_9 = ord('9')    
+    belowZero =False;
+    if str == "":
+        return False;
+    str_len = len(str);
+    
+    if str[0]=="-":
+        belowZero = True;
+
+    if str[0] == "+" or str[0]=="-":
+        str = str[1:];
+        str_len -=1;
+    if str_len == 0:
+        return False;
+
+    # change to number
+    for i in range(str_len):
+        if ord(str[i]) < a_0 or ord(str[i]) > a_9:
+            return False;
+        else:
+            
+            ret = 10*ret + int(str[i]);
+
+    if belowZero:
+        return -ret;
+    return ret;
+    
+print(myAtoi('-123'));
+
+
+
+
+# class Solution(object):  
+#     def myAtoi(self, str):  
+#         """ 
+#         :type str: str 
+#         :rtype: int 
+#         """  
+#         i = 0  
+#         sign = 1  
+#         base = 0  
+#         l = len(str)  
+#         INT_MAX = 2147483647  
+#         INT_MIN = -2147483648  
+#         a_0 = ord('0')  
+#         a_9 = ord('9')  
+#         while i < l and str[i] == ' ':  
+#             i += 1  
+#         if i < l and str[i] == '-':  
+#             sign = -1  
+#             i += 1  
+#         elif i< l and str[i] == '+':  
+#             i += 1  
+#         while i < l and ord(str[i]) >= a_0 and ord(str[i]) <= a_9:  
+#             if base > INT_MAX / 10 or (base == INT_MAX / 10 and ord(str[i]) - a_0 > 7):  
+#                 return sign == 1 and INT_MAX or INT_MIN  
+#             base = 10 * base + (ord(str[i]) - a_0)  
+#             i += 1  
+  
+#         return base * sign  

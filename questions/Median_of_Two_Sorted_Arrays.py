301af31fbddbf6d0000ee0cf60ce6b4feb9eb0c4
kobepeng <pengkobe123@gmail.com>
2017-08-08 12:48:31 +0800

    add final answer of median of 2 sorted arrays

--- questions/Median_of_Two_Sorted_Arrays.py
+++ questions/Median_of_Two_Sorted_Arrays.py
@@ -13,3 +13,34 @@
 # nums2 = [3, 4]
 
 # The median is (2 + 3)/2 = 2.5
+
+def median(nums1, nums2):
+    def getKth(A,B,kth):
+        print('getKth',A,B,kth)
+        if len(A) > len(B):
+            return getKth(B,A,kth);
+        if len(A) == 0:
+            return B[kth-1];
+        if kth == 1:
+            return min(A[0],B[0]);
+
+        pa = min(len(A), kth/2);
+        pb = kth - pa;
+        if A[pa-1] <= B[pb-1]:
+            return getKth(A[pa:],B,pb);
+        else:
+            return getKth(A,B[pb:],pa);        
+
+    len_nums1 = len(nums1);
+    len_nums2 = len(nums2);
+        
+    t_len = len_nums1 + len_nums2;
+    if t_len % 2 == 1:
+         return getKth(nums1,nums2,t_len/2 +1 );
+    else:
+        # /2 必须改为 * 0.5 否则四舍五入不会准的！！！！
+        return (getKth(nums1,nums2,t_len/2 +1 ) + getKth(nums1,nums2,t_len/2 )) /2;
+
+print(median([1, 3],[2]))
+print(median([2],[0]))
+print(median([2],[]))

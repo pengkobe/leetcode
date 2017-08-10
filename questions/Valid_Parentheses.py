256df64fc2659b820abe9ec5ccfcc1690223c743
kobepeng <pengkobe123@gmail.com>
2017-08-10 09:38:29 +0800

    add Valid_Parentheses( solution 1)

--- questions/Valid_Parentheses.py
+++ questions/Valid_Parentheses.py
@@ -2,5 +2,26 @@
 # The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 
 
+## 解法1，自认思路没有问题，但是字符串前面有 + U
 def isValid(s):
+    ret = [];
+    len_s = len(s) -1;
+    while len_s > 0:
+        if len(ret)==0 or ret[len(ret)-1] != s[len_s]:
+            ret.append(s[len_s])
+        else:
+            ret.pop();
+        len_s -= 1;
+    if len(ret) == 0:
+        return True;
+    else:
+        return False;
     return s;
+
+
+# 解法2
+def isValid2(s):
+    ret = [];
+    strs = ["(",")","[","]","{","}"];
+    
+    return len(ret) > 0;

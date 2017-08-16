38cd2b1282d1f4f7b8aec7a4360906aa2be13309
kobepeng <pengkobe123@gmail.com>
2017-08-16 10:16:49 +0800

    finish valid sudoku

--- questions/Valid_Sudoku.py
+++ questions/Valid_Sudoku.py
@@ -10,9 +10,35 @@
 # Note:
 # A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
 
-class Solution(object):
-    def isValidSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
+def isValidSudoku(self, board):
+    """
+    :type board: List[List[str]]
+    :rtype: bool
+    """
+    def isValid(row):
+        map =[];
+        for r in row:
+            if r in map:
+                return False;
+            else:
+                if r != ".":
+                    map.append(r);
+        return True;
+
+    ret = True;
+    for i in board: # 验证行
+        ret = isValid(board[i]);
+        if ret:
+            col = [c[i] for c in board];
+            ret = isValid(col); # 验证列
+            if not ret:
+                return False
+        else:
+            return False;
+
+    for m in [0,3,6]:
+        for n in [0,3,6]:
+            square = [board[s][t] for s in [i,n+1,n+2] for t in [m,m+1,m+2]];
+            ret = isValid(square);
+            if not ret:
+                return False

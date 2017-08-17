32afaad27fb3bbfa9d29eb18dcba963a8adc25ae
kobepeng <pengkobe123@gmail.com>
2017-08-17 09:09:31 +0800

    init Sudoku_Solver

---
+++ questions/Sudoku_Solver.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+# Empty cells are indicated by the character '.'.
+
+# You may assume that there will be only one unique solution.
+
+# A sudoku puzzle...
+
+# ...and its solution numbers marked in red.
+# https://leetcode.com/problems/sudoku-solver/description/

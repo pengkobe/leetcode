d3877aec6ac9757486924e48d1eb4f7fa0c722f0
kobepeng <pengkobe123@gmail.com>
2017-08-22 09:32:47 +0800

    finish human traffic of standium

--- questions/database/Human_Traffic_of_Stadium.sql
+++ questions/database/Human_Traffic_of_Stadium.sql
@@ -28,42 +28,65 @@
 -- Note:
 -- Each day only have one row record, and the dates are increasing with id increasing.
 
-select * from stadium a   where a.people >= 100 and 
+
+
+
+# Write your MySQL query statement below
+select a.* from stadium a where a.people >= 100 and
 (
     (
-        a.id+1 in  (select id from stadium where people >= 100) 
-        and 
-        a.id+2 in  (select id from stadium where people >= 100) 
+    a.id-2 in (select b.id from stadium b where b.people >= 100) and
+    a.id-1 in (select c.id from stadium c where c.people >= 100) 
     )
-    or 
+    or
     (
-        a.id-1  in  (select id from stadium where people >= 100) 
-        and 
-        a.id+1  in  (select id from stadium where people >= 100) 
+        a.id-1 in (select d.id from stadium d where d.people >= 100) and
+        a.id+1 in (select e.id from stadium e where e.people >= 100) 
     )
-    or 
+    or
     (
-        a.id-1  in  (select id from stadium where people >= 100) 
-        and 
-        a.id-2  in  (select id from stadium where people >= 100) 
+        a.id+1 in (select f.id from stadium f where f.people >= 100) and
+        a.id+2 in (select j.id from stadium j where j.people >= 100) 
     )
-);
+)
 
+-- 参考答案1
+-- select * from stadium a   where a.people >= 100 and 
+-- (
+--     (
+--         a.id+1 in  (select id from stadium where people >= 100) 
+--         and 
+--         a.id+2 in  (select id from stadium where people >= 100) 
+--     )
+--     or 
+--     (
+--         a.id-1  in  (select id from stadium where people >= 100) 
+--         and 
+--         a.id+1  in  (select id from stadium where people >= 100) 
+--     )
+--     or 
+--     (
+--         a.id-1  in  (select id from stadium where people >= 100) 
+--         and 
+--         a.id-2  in  (select id from stadium where people >= 100) 
+--     )
+-- );
 
-set @rownum1:=0;
-set @rownum2:=0;
-select temp1.id,temp1.date,temp1.people
-from (select count(*) as row_counts,diffnum
-    from (select t.*,t.id-t.row_num as diffnum
-            from (select a.*,
-                @rownum1:=@rownum1+1 as row_num
-                    from (select * from stadium  where people>=100) a) t)s
-group by diffnum) temp2
-join
-(select t.*,t.id-t.row_num as diffnum
- from (select a.*,
-   @rownum2:=@rownum2+1 as row_num
-     from (select * from stadium  where people>=100) a) t) temp1
-on (temp1.diffnum=temp2.diffnum)
-where temp2.row_counts>=3
-order by temp1.date
+-- 参考答案2
+-- set @rownum1:=0;
+-- set @rownum2:=0;
+-- select temp1.id,temp1.date,temp1.people
+-- from (select count(*) as row_counts,diffnum
+--     from (select t.*,t.id-t.row_num as diffnum
+--             from (select a.*,
+--                 @rownum1:=@rownum1+1 as row_num
+--                     from (select * from stadium  where people>=100) a) t)s
+-- group by diffnum) temp2
+-- join
+-- (select t.*,t.id-t.row_num as diffnum
+--  from (select a.*,
+--    @rownum2:=@rownum2+1 as row_num
+--      from (select * from stadium  where people>=100) a) t) temp1
+-- on (temp1.diffnum=temp2.diffnum)
+-- where temp2.row_counts>=3
+-- order by temp1.date
